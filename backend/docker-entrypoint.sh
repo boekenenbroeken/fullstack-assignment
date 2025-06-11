1700e6e148ee30fa17ebe34f7861b1ba44b74625
Ingrid <almost.bergman@gmail.com>
2025-06-11 15:56:59 +0200

    feat: proceed with npm to pnpm migration

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-# Exit immediately if a command exits with a non-zero status
 set -e
 
 echo "📦 Running Prisma migrations..."
 npx prisma migrate deploy
 
 echo "🌱 Running seed script..."
-npm run seed
+pnpm run seed
 
 echo "🚀 Starting the server..."
 exec "$@"
