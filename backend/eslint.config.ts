c1502ca300608d866543061be52649daa53e9582
Ingrid <almost.bergman@gmail.com>
2025-06-13 02:20:24 +0200

    chore: update esling config

--- backend/eslint.config.ts
+++ backend/eslint.config.ts
@@ -1,23 +1,25 @@
-import * as eslintPluginTs from '@typescript-eslint/eslint-plugin';
-import * as tsParser from '@typescript-eslint/parser';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
+  {
+    ignores: ['coverage/**', 'dist/**', 'src/generated'],
+  },
+
+  ...tseslint.configs.recommended,
+
   {
     files: ['**/*.ts'],
     languageOptions: {
-      parser: tsParser,
-      sourceType: 'module',
+      parserOptions: { project: './tsconfig.json' },
     },
     plugins: {
-      '@typescript-eslint': eslintPluginTs,
       prettier: eslintPluginPrettier,
       'simple-import-sort': simpleImportSort,
     },
     rules: {
       'prettier/prettier': 'error',
-      ...eslintPluginTs.configs.recommended.rules,
       'padding-line-between-statements': [
         'error',
         { blankLine: 'always', prev: '*', next: 'return' },
@@ -26,7 +28,4 @@ export default [
       'simple-import-sort/exports': 'error',
     },
   },
-  {
-    ignores: ['coverage/**', 'dist/**', 'src/generated'],
-  },
-];
+);
